da6d76621c165283817b52f197d08e6a7801c6d0
version-1 <j.onodera26@gmail.com>
2020-09-06 19:35:05 -0700

    impl transition

--- javascript/js/function.js
+++ javascript/js/function.js
@@ -5,11 +5,19 @@ const context = {
   score: 0,
   count: 1,
   answer: null,
+  playerName: '',
+  playerNameElement: document.querySelector('.player-name'),
   playerElement: document.querySelector('#player'),
   playButton: document.querySelector('.play'),
+  startButton: document.querySelector('.start-button'),
   scaleButtons: document.querySelectorAll('.sound'),
   scoreText: document.querySelector('.score'),
-  answerCount: document.querySelector('.answer-count')
+  answerCount: document.querySelector('.answer-count'),
+  scenes: {
+    start: document.querySelector('.start-scene'),
+    game: document.querySelector('.game-scene'),
+    result: document.querySelector('.result-scene')
+  }
 }
 
 const soundFiles = [
@@ -17,7 +25,7 @@ const soundFiles = [
   '02_re',
   '03_mi',
   '04_fa',
-  '05_do',
+  '05_so',
   '06_ra',
   '07_shi'
 ]
@@ -34,11 +42,14 @@ const updateAnswerCount = () => {
   context.answerCount.innerText = Math.min(context.count, PROBLEM_COUNT)
 }
 
+const updatePlayerName = () => {
+  context.playerNameElement.innerText = context.playerName
+}
+
 const makeQuestion = () => {
   context.answer = Math.floor(Math.random() * soundFiles.length)
   const fileName = soundFiles[context.answer]
   context.playerElement.setAttribute('src', `assets/mp3/${fileName}.mp3`)
-
 }
 
 const checkAnswer = (ans) => {
@@ -52,23 +63,48 @@ const checkAnswer = (ans) => {
   return soundFiles[answer] === ans
 }
 
-
 const subscribe = () => {
+  context.startButton.addEventListener('click', () => {
+    context.playerName = document.querySelector('.input').value
+    if (!context.playerName) {
+      alert('プレイヤー名を入力してください')
+      return
+    }
+    moveToGame()
+  })
+}
+
+const moveToGame = () => {
+  const { start, game } = context.scenes
+  start.classList.add('is-hidden')
+  game.classList.remove('is-hidden')
+
+  onLoadGameScene()
+}
+
+const moveToResult = () => {
+  const { game, result } = context.scenes
+  game.classList.add('is-hidden')
+  result.classList.remove('is-hidden')
+
+  onLoadResultScene()
+}
+
+const onLoadGameScene = () => {
   const { playerElement, playButton, scaleButtons } = context
+  updatePlayerName()
   makeQuestion()
   updateScore()
   updateAnswerCount()
 
   playButton.addEventListener('click', () => {
-    if (context.count > PROBLEM_COUNT) {
-      return
-    }
     playerElement.play()
   })
 
   scaleButtons.forEach(item => {
     item.addEventListener('click', (e) => {
       if (context.count > PROBLEM_COUNT) {
+        moveToResult()
         return
       }
       const ans = e.target.getAttribute('data-key')
@@ -80,6 +116,10 @@ const subscribe = () => {
       }
     })
   })
+}
+
+const onLoadResultScene = () => {
+
 }
 
 subscribe()
