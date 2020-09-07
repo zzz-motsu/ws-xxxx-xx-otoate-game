f06482ec2bb0bf8a23e97e0c563428c5ebe126b0
version-1 <j.onodera26@gmail.com>
2020-09-06 17:38:18 -0700

    create api

--- server/README.md
+++ server/README.md
@@ -1,5 +1,6 @@
 
 
+https://qiita.com/FrogWoman/items/f8a70286c7f1c4d5fc02
 
 https://expressjs.com/ja/
 
@@ -7,3 +8,7 @@ https://expressjs.com/ja/
 https://mongoosejs.com/docs/index.html
 
 
+```
+brew tap mongodb/brew
+brew install mongodb-community@4.4
+```
--- server/index.js
+++ server/index.js
@@ -1,10 +1,72 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express()
+const { Score } = require('./models')
+
 
 const port = 3000
 
-app.get('/', function (req, res) {
-  res.json({ message: 'ok' })
+const scoresPath = '/scores'
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.get('/', function (_req, res) {
+  res.send(`
+    <h1>Otoate API</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Path</th>
+          <th>Method</th>
+          <th>Reponse Type</th>
+          <th>Memo</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>/</td>
+          <td>GET</td>
+          <td>HTML</td>
+          <td>top page</td>
+        </tr>
+        <tr>
+          <td>/scores</td>
+          <td>GET</td>
+          <td>JSON</td>
+          <td>get score list</td>
+        </tr>
+        <tr>
+          <td>/scores</td>
+          <td>POST</td>
+          <td>JSON</td>
+          <td>create score</td>
+        </tr>
+      </tbody>
+    </table>
+  `)
+})
+
+app.get(scoresPath, function (_req, res) {
+  Score.find({}, function (err, result) {
+    if (err) {
+      throw err
+    }
+    res.json(result)
+  })
+})
+
+app.post(scoresPath, function (req, res) {
+  const score = new Score(req.body)
+  score.save(function (err) {
+    if (err) {
+      throw err
+    }
+    res.status(201).json()
+  })
 })
 
 app.listen(port, () => {
