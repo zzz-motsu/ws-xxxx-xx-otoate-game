849c08563043e9378656d94e623d01c38e9abfe9
Takeshi Okamoto <takeshi_okamoto@MacBook-Pro.local>
2020-11-27 02:15:06 +0900

    creat-score

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const { db, Score } = require('./models')
+const port = 3000
+
+// CORSを許可する
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/scores', (req, res) => {
+  Score.find({}, function(err, result){
+    if (err) throw err;
+    console.log(result);
+    res.json(result)
+  });
+})
+
+app.post('/scores', (req, res) => {
+  res.json({})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
---
+++ server/models/index.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+// var constants = require('./lib/constants');
+mongoose.connect('mongodb://localhost/otoate', { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function() {
+  console.log('connect!!!')
+});
+
+const Schema = mongoose.Schema;
+
+// スキーマ
+const scoreSchema = new Schema({
+  name: String,
+  score: Number
+});
+
+const Score = mongoose.model('Score', scoreSchema)
+
+module.exports = {
+  db,
+  Score
+}
