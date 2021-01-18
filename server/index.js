e9dd7405ad7e37dd55a69f0808f03a946afe41a1
Takeshi Okamoto <takeshi_okamoto@MacBook-Pro.local>
2021-01-18 12:05:36 +0900

    server/index.js

--- server/index.js
+++ server/index.js
@@ -23,10 +23,6 @@ app.use(function(req, res, next) {
 
 app.use(express.static('front-js'));
 
-app.get("/", (req, res) =>{
-  res.sendFile(__dirname + "/front-js/index.html");
-});
-
 app.get('/scores', (req, res) => {
   Score.find({}, function(err, result){
     if (err) throw err;
